e066618ae58d8b70c64598f31d88d665c1483ed3
dphdmn <58388093+dphdmn@users.noreply.github.com>
2022-11-12 22:23:01 +0800

    Add files via upload

--- statsAnalyser.py
+++ statsAnalyser.py
@@ -33,36 +33,74 @@ def calibriFont(size):
     return ImageFont.truetype("calibri_font.ttf", size)
 
 
+def getPartsAverages(fd):
+    avgs = []
+    effs = []
+    for part in range(0, 8):
+        sumValids = 0
+        sumTimes = 0
+        sumEffs = 0
+        for i in range(10 * part - 1, 10 * part + 9):
+            if fd[i]["empty"] == False:
+                sumValids += 1
+                sumTimes += fd[i]["time"]
+                sumEffs += fd[i]["efftps"]
+        if sumValids > 0:
+            avgs.append(round(sumTimes / sumValids, 3))
+            effs.append(round(sumEffs / sumValids, 3))
+        else:
+            avgs.append(-1)
+            effs.append(-1)
+    return avgs, effs
+
+
+def getNormalColor(fastest, slowest, timelist):
+    timecolors = {}
+    grad = getRYBgradient([20, 20, 20, 20])
+    for time in timelist:
+        normalized = int(79 * (time - fastest) / slowest)
+        if normalized > 79:
+            normalized = 79
+        if normalized < 0:
+            normalized = 0
+        timecolors[time] = grad[normalized]
+    return timecolors
+
+
 def drawImage(fd, res):
+    parts, avgeffs = getPartsAverages(fd)
     W = res
-    H = res
+    H = int(res * 1.4)
     image = Image.new("RGBA", (W, H), (255, 255, 255))
     draw = ImageDraw.Draw(image)
-    margin = res / 12
-    yshift = res / 10
+    margin = res / 5
+    yshift = -res / 3
     xshift = 0
-    cellsize = res / 14
+    cellsize = res / 11.5
     fontsizeTime = int(cellsize / 3)
     fontsizeHeaders = int(cellsize / 4)
+    fontsizeVerySmall = int(cellsize / 6)
     timeFont = calibriFont(fontsizeTime)
     headersFont = calibriFont(fontsizeHeaders)
+    verySmalFont = calibriFont(fontsizeVerySmall)
     outlineW = int(cellsize / 30)
     startX, startY = margin + xshift, margin + yshift
+    gapshift = res / 100
+    headers = []
+    headers10 = []
+    for i in range(1, 17):
+        if i == 1:
+            head = "1 - 4"
+        else:
+            head = '{fr} - {t}'.format(fr=i * 5 - 5, t=i * 5 - 1)
+        headers.append(head)
     for i in range(1, 9):
         if i == 1:
             head = "1 - 9"
         else:
             head = '{fr} - {t}'.format(fr=i * 10 - 10, t=i * 10 - 1)
-        x = startX - cellsize
-        y = startY + cellsize * (i - 1)
-        textX = x + cellsize / 2
-        textY = y + cellsize / 2 + fontsizeHeaders / 3
-        draw.text((textX, textY),
-                  head,
-                  font=headersFont,
-                  fill='black',
-                  align='center',
-                  anchor='ms')
+        headers10.append(head)
+    print(headers10)
     validtimes = []
     for i in fd:
         if not i["empty"]:
@@ -70,25 +108,19 @@ def drawImage(fd, res):
     fastest = min(validtimes)
     average = sum(validtimes) / len(validtimes)
     slowest = 2 * (average - fastest)
-    timecolors = {}
-    grad = getRYBgradient([20, 20, 20, 20])
-    for time in validtimes:
-        normalized = int(79 * (time - fastest) / slowest)
-        if normalized > 79:
-            normalized = 79
-        timecolors[time] = grad[normalized]
-    for i in range(1, 80):
-        print(i)
+    timecolors = getNormalColor(fastest, slowest, validtimes)
+    partcolors = getNormalColor(fastest, slowest, parts)
+    for i in range(10, 80):
         data = fd[i - 1]
         valid = not data["empty"]
         if valid:
             bgcolor = timecolors[data["time"]]
         else:
             bgcolor = 'gray'
-        x = startX + cellsize * (i % 10)
-        y = startY + cellsize * (i // 10)
-        xl = startX + cellsize + cellsize * (i % 10)
-        yl = startY + cellsize + cellsize * (i // 10)
+        x = startX + cellsize * (i % 5)
+        y = startY + cellsize * (i // 5) + gapshift * (i // 10)
+        xl = startX + cellsize + cellsize * (i % 5)
+        yl = startY + cellsize + cellsize * (i // 5) + gapshift * (i // 10)
         draw.rectangle(
             xy=(x, y, xl, yl),
             fill=bgcolor,
@@ -103,6 +135,68 @@ def drawImage(fd, res):
                       fill=goodTextColor(bgcolor),
                       align='center',
                       anchor='ms')
+            draw.text((textX, textY + cellsize * 0.3),
+                      "best of " + str(data["solvesAmount"]),
+                      font=verySmalFont,
+                      fill=goodTextColor(bgcolor),
+                      align='center',
+                      anchor='ms')
+            draw.text((textX, textY - cellsize * 0.4),
+                      "eff. " + str(data["efftps"]),
+                      font=verySmalFont,
+                      fill=goodTextColor(bgcolor),
+                      align='center',
+                      anchor='ms')
+            draw.text((textX, textY - cellsize * 0.25),
+                      "+" + str(data["dif"]),
+                      font=verySmalFont,
+                      fill=goodTextColor(bgcolor),
+                      align='center',
+                      anchor='ms')
+        if i % 5 == 0 or i == 1:
+            headerX = startX - cellsize + cellsize / 2
+            headerY = textY
+            draw.text((headerX, headerY),
+                      headers[i // 5],
+                      font=headersFont,
+                      fill='black',
+                      align='center',
+                      anchor='ms')
+        if (i + 1) % 10 == 0:
+            partX = x + cellsize + gapshift
+            partXl = partX + 2 * cellsize
+            partY = y - cellsize
+            partYl = yl
+            ptid = i // 10
+            pt = parts[ptid]
+            if pt != -1:
+                bgcolor = partcolors[pt]
+            draw.rectangle(
+                xy=(partX, partY, partXl, partYl),
+                fill=bgcolor,
+                outline='black',
+                width=outlineW)
+            textX = partX + cellsize
+            textY = partY + cellsize + fontsizeTime / 3
+            if pt != -1:
+                draw.text((textX, textY),
+                          str(pt),
+                          font=timeFont,
+                          fill=goodTextColor(bgcolor),
+                          align='center',
+                          anchor='ms')
+                draw.text((textX, textY - cellsize * 0.7),
+                          "Avg eff. " + str(avgeffs[ptid]),
+                          font=headersFont,
+                          fill=goodTextColor(bgcolor),
+                          align='center',
+                          anchor='ms')
+                draw.text((textX, textY + cellsize * 0.6),
+                          "Solves " + headers10[ptid],
+                          font=headersFont,
+                          fill=goodTextColor(bgcolor),
+                          align='center',
+                          anchor='ms')
     image.show()
 
 
@@ -132,6 +226,7 @@ def analyseOptimal(optimalStats):
             el["dif"] = min["mvc"] - i
             el["tps"] = round(min["mvc"] / min["time"], 3)
             el["efftps"] = round(i / min["time"], 3)
+            print(el)
         freqData.append(el)
     return freqData
 
